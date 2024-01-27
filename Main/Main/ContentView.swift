7df6642d909b448e9ae200bdf00c9f498b8c9cb2
Jyun Rong Liu <jyunrl@uci.edu>
2024-01-26 19:26:47 -0800

    project setup

---
+++ Main/Main/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Main
+//
+//  Created by James Liu on 2024/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Main/Main/MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainApp.swift
+//  Main
+//
+//  Created by James Liu on 2024/1/26.
+//
+
+import SwiftUI
+
+@main
+struct MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
